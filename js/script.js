cd0a57c656a5e507a4e0cbea47293c5ed7bdac43
syuhada-ramadhan <suhadahada18@gmail.com>
2021-12-15 09:51:38 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// round
+const bilangan = 1.9;
+const hasil = Math.round(bilangan);
+console.log(hasil);
+
+// ceil
+const bilangan = 1.9;
+const hasil = Math.ceil(bilangan);
+console.log(hasil);
+
+// floor
+const bilangan = 1.9;
+const hasil = Math.floor(bilangan);
+console.log(hasil);
+
+// min
+const bilangan = [1, 4, 5, 8, 10];
+const hasil = Math.min(...bilangan);
+console.log(hasil);
+
+// max
+const bilangan = [1, 4, 5, 8, 10];
+const hasil = Math.max(...bilangan);
+console.log(hasil);
+
+// random
+const nomor = Math.random();
+console.log(nomor);
